243789a5d66a6d31a03f49c916e60584a4096d02
Student <student@Barrys-MacBook-Air.local>
2016-10-23 12:19:43 -0400

    made updates

--- story.rb
+++ story.rb
@@ -5,13 +5,19 @@
 
 
 #show the user a message that says "Welcome to Movie Rating"
+puts("Welcome to Movie Rating")
 
 #show the user a message asking them for the title of a movie
+puts("Name a movie title")
 
 #store the input in a variable
+user_title = gets.strip
 
 #show the user a message asking for their rating of the movie out of ten
+puts("What rating out of ten would you give the movie?")
 
 # store the input in a variable
+user_rating = gets.strip
 
 display_story(user_title, user_rating)
+puts("You gave #{user_title} a #{user_rating} out of ten.)
